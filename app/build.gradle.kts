5e2291320585bc22c245321f54ffab5039df7bea
kasia <ul0275820@edu.uni.lodz.pl>
2024-03-19 00:38:43 +0100

    wybranie pliku z galerii do zmiany avatara

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore-ktx:24.10.3")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("com.google.firebase:firebase-storage:20.0.0")
+    implementation("com.squareup.picasso:picasso:2.71828")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/myapplication2/profil.kt
+++ app/src/main/java/com/example/myapplication2/profil.kt
@@ -1,34 +1,89 @@
 package com.example.myapplication2
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class profil : AppCompatActivity() {
     private val chmura = FirebaseFirestore.getInstance()
     private lateinit var userEmailTextView: TextView
+    private lateinit var storageReference: StorageReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profil)
 
-        // Znajdź TextView w układzie
         userEmailTextView = findViewById(R.id.viewMail)
+        storageReference = FirebaseStorage.getInstance().reference
 
-        // Pobierz aktualnie zalogowanego użytkownika z Firebase Authentication
+        val changeAvatarButton: Button = findViewById(R.id.zmiana)
+        changeAvatarButton.setOnClickListener {
+            chooseImageFromGallery()
+        }
+
         val currentUser = FirebaseAuth.getInstance().currentUser
 
-        // Sprawdź, czy użytkownik jest zalogowany
         if (currentUser != null) {
-            // Pobierz adres e-mail użytkownika
             val userEmail = currentUser.email
-            // Wyświetl adres e-mail użytkownika w TextView
             userEmailTextView.text = userEmail
         } else {
-            // Jeśli użytkownik nie jest zalogowany
             userEmailTextView.text = "Brak zalogowanego użytkownika"
         }
     }
+
+    private fun chooseImageFromGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    private val PICK_IMAGE_REQUEST = 1
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.data != null) {
+            val imageUri = data.data
+            if (imageUri != null) {
+                uploadImageToFirebaseStorage(imageUri)
+            } else {
+                Toast.makeText(this, "Nie udało się uzyskać adresu URI pliku", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    private fun uploadImageToFirebaseStorage(imageUri: Uri) {
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            val fileName = "avatar_${currentUser.uid}.jpg"
+            val fileRef = storageReference.child("avatars").child(fileName)
+            val uploadTask = fileRef.putFile(imageUri)
+            uploadTask.addOnSuccessListener {
+
+                //pobierz adres URL przesłanego zdjęcia i zapisz go w bazie danych Firestore
+                fileRef.downloadUrl.addOnSuccessListener { uri ->
+                    val userDocRef = chmura.collection("users").document(currentUser.uid)
+                    userDocRef.update("avatarUrl", uri.toString())
+                        .addOnSuccessListener {
+                            Toast.makeText(this, "Zmiana avatara zakończona pomyślnie", Toast.LENGTH_SHORT).show()
+                        }
+                        .addOnFailureListener { e ->
+                            Toast.makeText(this, "Wystąpił błąd podczas zmiany avatara: ${e.message}", Toast.LENGTH_SHORT).show()
+                        }
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(this, "Wystąpił błąd podczas przesyłania zdjęcia: ${exception.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
 }
+
